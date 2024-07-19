8da29e1d1a6281dda99c9a88311fed8bc6029f45
ByBoS001 <brortegasa@uide.edu.ec>
2024-07-19 08:57:53 -0500

    semana 12

---
+++ src/app/components.tsx
@@ -0,0 +1,19 @@
+function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    );
+  }
+
+export default function components(){
+    return(
+        <section>
+            <Profile>
+                
+            </Profile>
+        </section>
+    )
+        
+};
